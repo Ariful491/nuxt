650b6d5bdad9fab2c4d3f93cfcabed19100ed317
Ariful491 <arifulhoque091@gmail.com>
2023-10-29 23:57:11 +0600

    start navigation.

--- app.vue
+++ app.vue
@@ -5,5 +5,10 @@
       <a href="" class="text-2xl font-mono uppercase font-bold"> Car Mover</a>
     </header>
     <!-- NAVBAR -->
+    <!-- HOME HERO-->
+    <div class="relative h-screen w-100 bg-no-repeat bg-cover bg-bottom bg-[url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfC0kl7MvHX_M6BdFzs2gmXlSr5RG4cR4oNAwNBuptBPXFeHHfjmZnVVensNotOSjt4oY&usqp=CAU')]">
+
+    </div>
+    <!-- HOME HERO-->
   </div>
 </template>
