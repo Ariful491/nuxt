d7146acc0141e6a1df02318f28b0f915e404dd43
Ariful491 <arifulhoque091@gmail.com>
2023-11-01 23:25:50 +0600

    car details .

--- app.vue
+++ app.vue
@@ -1,13 +1,7 @@
+
 <template>
   <div>
-    <!-- NAVBAR -->
-    <header class="sticky top-0 z-50  flex justify-between space-x-1 shadow-md p-4  items-center border-b bg-white">
-      <a href="" class="text-2xl font-mono uppercase font-bold"> Car Mover</a>
-    </header>
-    <!-- NAVBAR -->
-
-
+    <NavBar/>
+    <NuxtPage/>
   </div>
 </template>
-<script setup lang="ts">
-</script>
