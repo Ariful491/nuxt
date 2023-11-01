ff328a55f73f45f4d230b19e826158a9283e5912
Ariful491 <arifulhoque091@gmail.com>
2023-11-01 23:34:50 +0600

    car details .

---
+++ components/Car/Hero.vue
@@ -0,0 +1,23 @@
+
+<template>
+  <div
+      class="relative h-screen w-100 bg-no-repeat bg-cover bg-bottom bg-[url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfC0kl7MvHX_M6BdFzs2gmXlSr5RG4cR4oNAwNBuptBPXFeHHfjmZnVVensNotOSjt4oY&usqp=CAU')]">
+    <div class="z-10 absolute w-full h-full flex justify-center items-center">
+      <div class="text-center w-1/2">
+        <h1 class="text-8xl font-serif mb-10 text-white">
+          Find Your Driver
+        </h1>
+        <!-- HOME SEARCH BAR -->
+          <CarSearchBar/>
+        <!-- HOME SEARCH BAR -->
+      </div>
+    </div>
+    <div class="z-1 absolute w-full h-full bg-black opacity-50">
+
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ components/NavBar.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header class="sticky top-0 z-50  flex justify-between space-x-1 shadow-md p-4  items-center border-b bg-white">
+    <a href="" class="text-2xl font-mono uppercase font-bold"> Car Mover</a>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
