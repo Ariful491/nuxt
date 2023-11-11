cf8bab6e8620423abd719853420d5b7672b0f177
Ariful491 <arifulhoque091@gmail.com>
2023-11-11 20:21:15 +0600

    Car detail Page

--- pages/car/[name]-[id].vue
+++ pages/car/[name]-[id].vue
@@ -1,33 +1,41 @@
 <script setup lang="ts">
+import {definePageMeta} from "#imports";
+
 const route = useRoute();
 
 useHead({
   title: `${route.params.name}`
 })
+
+definePageMeta({
+  layout: "custom"
+})
 </script>
 
 <template>
   <!-- CAR DETAIL PAGE -->
-  <div class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5">
+
     <div>
       <!-- CAR HERO -->
       <CarDetailHero/>
       <!-- CAR HERO -->
+
+      <!-- CAR ATTRIBUTES -->
+      <CarDetailAttributes/>
+      <!-- CAR ATTRIBUTES -->
+
+      <!-- CAR DESCRIPTION -->
+      <CarDetailDescription/>
+      <!-- CAR DESCRIPTION -->
+
+      <!-- CAR CONTACT -->
+      <CarDetailContact/>
+      <!-- CAR CONTACT -->
+
     </div>
 
 
-    <!-- CAR ATTRIBUTES -->
-    <CarDetailAttributes/>
-    <!-- CAR ATTRIBUTES -->
 
-    <!-- CAR DESCRIPTION -->
-    <CarDetailDescription/>
-    <!-- CAR DESCRIPTION -->
-
-    <!-- CAR CONTACT -->
-    <CarDetailContact/>
-    <!-- CAR CONTACT -->
-  </div>
   <!-- CAR DETAIL PAGE   -->
 </template>
 
