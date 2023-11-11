032e9a933d087583e1c93b208e0786320b4440dd
Ariful491 <arifulhoque091@gmail.com>
2023-11-11 17:02:37 +0600

    Car detail Page

---
+++ pages/car/[name]-[id].vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <!-- CAR DETAIL PAGE -->
+  <div class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5">
+    <div>
+      <!-- CAR HERO -->
+      <CarDetailHero/>
+      <!-- CAR HERO -->
+    </div>
+
+
+    <!-- CAR ATTRIBUTES -->
+    <CarDetailAtrributes/>
+    <!-- CAR ATTRIBUTES -->
+
+    <!-- CAR DESCRIPTION -->
+   <CarDetailDescription/>
+    <!-- CAR DESCRIPTION -->
+
+    <!-- CAR CONTACT -->
+   <CarDetailContact/>
+    <!-- CAR CONTACT -->
+  </div>
+  <!-- CAR DETAIL PAGE   -->
+</template>
+
+<style scoped>
+
+</style>
