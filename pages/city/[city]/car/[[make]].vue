5cf3597ebd05a480ca3dbe58b59bd11d635c2c59
Ariful491 <arifulhoque091@gmail.com>
2023-11-11 15:01:43 +0600

    created card page.

--- pages/city/[city]/car/[[make]].vue
+++ pages/city/[city]/car/[[make]].vue
@@ -6,30 +6,8 @@
       <!-- CAR SIDE BAR     -->
      <CarSideBar/>
       <!-- CAR SIDE BAR     -->
-
-
       <!-- CAR CARDS     -->
-      <div class="w-full">
-        <!-- CAR CARD          -->
-        <div class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]">
-          <div class="flex h-full">
-            <img src="https://assets.fastly.carvana.io/home-assets/nba/civic.png" alt="" class="w-[300px] h-full">
-            <div class="p-4 flex flex-col">
-              <div>
-                <h1 class="text-2xl text-blue-700">
-                  Volvo XC40
-                </h1>
-                <p class="text-gray-700">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                </p>
-                <h1 class=" mt-12 text-xl">
-                  $39,500
-                </h1>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+     <CarCard/>
       <!-- CAR CARDS     -->
     </div>
   </div>
