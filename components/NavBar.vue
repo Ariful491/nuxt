c5d4ccc62e4c4860f9b7c9d290f9b00889d1e7ee
Ariful491 <arifulhoque091@gmail.com>
2023-11-11 17:09:53 +0600

    Car detail Page

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -4,7 +4,7 @@
 
 <template>
   <header class="sticky top-0 z-50  flex justify-between space-x-1 shadow-md p-4  items-center border-b bg-white">
-    <a href="" class="text-2xl font-mono uppercase font-bold"> Car Mover</a>
+    <NuxtLink to="/" class="text-2xl font-mono uppercase font-bold"> Car Mover</NuxtLink>
   </header>
 </template>
 
