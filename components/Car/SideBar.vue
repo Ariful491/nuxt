01c9a7f2f85a5d6d95961f257ee529f295d88674
Ariful491 <arifulhoque091@gmail.com>
2023-11-11 14:55:55 +0600

    create city pages.

--- components/Car/SideBar.vue
+++ components/Car/SideBar.vue
@@ -20,9 +20,11 @@
       <h3>
         INDIA
       </h3>
+
       <h3 class="text-blue-400 capitalize">
         BOMBAY
       </h3>
+
     </div>
     <div class="p-5 flex justify-between relative cursor-pointer border-b">
       <h3>
