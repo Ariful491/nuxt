5cdbf8314091285b690bae3e506e8fbe40e582bf
Ariful491 <arifulhoque091@gmail.com>
2023-11-16 21:35:36 +0600

    Car detail Page

--- pages/city/[city]/car.vue
+++ pages/city/[city]/car.vue
@@ -11,9 +11,11 @@ useHead({
 <template>
   <div class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16">
     <div class="mt-32 flex">
+
       <!-- CAR SIDE BAR     -->
       <CarSideBar/>
       <!-- CAR SIDE BAR     -->
+
       <!-- CAR CARDS     -->
       <NuxtPage/>
       <!-- CAR CARDS     -->
