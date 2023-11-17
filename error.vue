f038a8c10556f25ff312059868296070d62717f2
Ariful491 <arifulhoque091@gmail.com>
2023-11-17 18:25:16 +0600

    Car detail Page

--- error.vue
+++ error.vue
@@ -1,4 +1,5 @@
 <script setup>
+const error = useError();
 const handleReturn = () => {
   navigateTo('/');
 }
@@ -6,8 +7,8 @@ const handleReturn = () => {
 
 <template>
   <div class="flex h-screen justify-center items-center flex-col">
-    <h1 class="text-9xl font-semibold">404</h1>
-    <p class="mt-7 text-4xl"> page not found.</p>
+    <h1 class="text-9xl font-semibold">{{ error.statusCode }}</h1>
+    <p class="mt-7 text-4xl"> {{error.message}}</p>
     <button @click="handleReturn" class="rounded  mt-7 text-1xl bg-blue-900 px-7 py-4 text-white"> GO BACK</button>
   </div>
 </template>
