4ef534e7c06a7d680d54f8814577c8b8914cb679
Ariful491 <arifulhoque091@gmail.com>
2023-12-03 23:23:45 +0600

    clear error handler.

--- components/Car/SideBar.vue
+++ components/Car/SideBar.vue
@@ -9,12 +9,12 @@
       <h3 class="text-blue-400 capitalize">
         Dhaka
       </h3>
-<!--      <div class="absolute border shadow left-56 p-5 top-1 -m-1 bg-white">
+      <div class="absolute border shadow left-56 p-5 top-1 -m-1 bg-white">
         <input type="text" class="border focus:outline-none p-1 rounded"/>
         <button class="bg-blue-400  w-full mt-2 rounded text-white p-1">
           Apply
         </button>
-      </div>-->
+      </div>
     </div>
     <div class="p-5 flex justify-between relative cursor-pointer border-b">
       <h3>
