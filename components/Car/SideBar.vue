44aa2c9a83ad4f40281ec2b8670405680424765e
Ariful491 <arifulhoque091@gmail.com>
2023-12-10 23:20:08 +0600

    clear error handler.

--- components/Car/SideBar.vue
+++ components/Car/SideBar.vue
@@ -1,4 +1,14 @@
 <script setup lang="ts">
+const modal = ref({
+  make: false,
+  location: false,
+  price: false
+})
+
+const updateModal = (variable: any ) => {
+  //modal.value[variable]
+}
+
 </script>
 <template>
   <div class="shadow border w-64 mr-10 z-30 h-[190px]">
@@ -6,15 +16,17 @@
       <h3>
         Location
       </h3>
-      <h3 class="text-blue-400 capitalize">
+      <h3 @click="updateModal('location')" class="text-blue-400 capitalize">
         Dhaka
       </h3>
-      <div class="absolute border shadow left-56 p-5 top-1 -m-1 bg-white">
+
+      <div v-if="modal.location" class="absolute border shadow left-56 p-5 top-1 -m-1 bg-white">
         <input type="text" class="border focus:outline-none p-1 rounded"/>
         <button class="bg-blue-400  w-full mt-2 rounded text-white p-1">
           Apply
         </button>
       </div>
+
     </div>
     <div class="p-5 flex justify-between relative cursor-pointer border-b">
       <h3>
@@ -32,7 +44,6 @@
         500/=
       </h3>
     </div>
-
   </div>
 </template>
 
