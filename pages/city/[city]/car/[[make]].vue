88bf642333f37845ad8fd7e2bfcd9c3b70f36d38
Ariful491 <arifulhoque091@gmail.com>
2023-12-13 00:23:14 +0600

    use vue for local storage

--- pages/city/[city]/car/[[make]].vue
+++ pages/city/[city]/car/[[make]].vue
@@ -8,19 +8,29 @@ interface Favorite {
   id: number
 }
 
-const favorite = useLocalStorage<Favorite>("favorites", {})
+const favorites = useLocalStorage<Favorite>("favorites",  [])
 
 const handleFavorite = (id: number) => {
-  const handleFavorite = (id: number) => {
-    const isFavorite = favorites.value.some((fav) => fav.id === id);
 
-    if (isFavorite) {
-      // If the car is already in favorites, remove it
-      favorites.value = favorites.value.filter((fav) => fav.id !== id);
-    } else {
-      // If the car is not in favorites, add it
-      favorites.value.push({ id });
-    }
+  const isFavorite = favorites.value.some((fav: { id: number; }) => fav.id === id);
+
+  if (isFavorite) {
+    // If the car is already in favorites, remove it
+    favorites.value = favorites.value.filter((fav: { id: number; }) => fav.id !== id);
+  } else {
+    // If the car is not in favorites, add it
+    favorites.value.push({id});
+
+  }
+
+
+/*  if (id in favorite.value) {
+    delete favorite[id]
+  } else {
+    favorite.value == {
+      ...favorite.value, ['id']: true
+    }*/
+
 }
 </script>
 <template>
